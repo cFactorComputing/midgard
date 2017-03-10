5571b222669e7d1ced14442c7428446702e8e09c
Anoop K John <anoop.kjohn@swiftwallet.io>
2017-03-11 00:21:07 +0530

    Gateway - Retrieve authenticated user

--- midgard-core/src/main/java/io/swiftwallet/midgard/common/user/CurrentUserFacade.java
+++ midgard-core/src/main/java/io/swiftwallet/midgard/common/user/CurrentUserFacade.java
@@ -1,6 +1,8 @@
 package io.swiftwallet.midgard.common.user;
 
+import io.swiftwallet.common.domain.security.AuthenticatedUser;
 import io.swiftwallet.common.domain.security.WalletUser;
+import io.swiftwallet.common.persistence.cache.repository.security.AuthenticatedUserCache;
 import io.swiftwallet.common.persistence.cache.repository.user.WalletUserCache;
 import io.swiftwallet.common.util.security.provider.AuthenticatedUserProvider;
 import org.springframework.security.core.userdetails.User;
@@ -15,9 +17,16 @@ public class CurrentUserFacade {
     private AuthenticatedUserProvider userProvider;
     @Inject
     private WalletUserCache walletUserCache;
+    @Inject
+    private AuthenticatedUserCache authenticatedUserCache;
 
     public WalletUser retrieveCurrentUser() {
         final User user = userProvider.user();
         return walletUserCache.findOne(user.getUsername());
+    }
+
+    public AuthenticatedUser retrieveAuthenticatedUser() {
+        final User user = userProvider.user();
+        return authenticatedUserCache.findByUserId(user.getUsername());
     }
 }
