27ef1bc51d49ee81d9a5ed4e714a1c3d50802aa7
Anoop K John <anoop.kjohn@swiftwallet.io>
2017-03-21 00:01:49 +0530

    Delete all existing tokens by userId

--- midgard-core/src/main/java/io/swiftwallet/midgard/common/auth/command/AuthenticatedUserController.java
+++ midgard-core/src/main/java/io/swiftwallet/midgard/common/auth/command/AuthenticatedUserController.java
@@ -1,5 +1,6 @@
 package io.swiftwallet.midgard.common.auth.command;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,12 +10,16 @@ import javax.inject.Inject;
 
 @RestController
 public class AuthenticatedUserController {
-
-    @Inject
     private AuthenticatedUserFacade authenticatedUserFacade;
 
+    @Inject
+    public AuthenticatedUserController(final AuthenticatedUserFacade authenticatedUserFacade) {
+        this.authenticatedUserFacade = authenticatedUserFacade;
+    }
+
+    @PreAuthorize("#oauth2.hasScope('write') and hasRole('ROLE_USER')")
     @RequestMapping(value = "/command/logout", method = RequestMethod.POST)
-    private void logout(@RequestParam("access_token") final String accessToken){
+    private void logout(@RequestParam("access_token") final String accessToken) {
         authenticatedUserFacade.logout(accessToken);
     }
 
--- midgard-core/src/main/java/io/swiftwallet/midgard/common/auth/command/AuthenticatedUserFacade.java
+++ midgard-core/src/main/java/io/swiftwallet/midgard/common/auth/command/AuthenticatedUserFacade.java
@@ -1,5 +1,6 @@
 package io.swiftwallet.midgard.common.auth.command;
 
+import io.swiftwallet.common.domain.security.AuthenticatedUser;
 import io.swiftwallet.common.persistence.cache.repository.security.AuthenticatedUserCache;
 import io.swiftwallet.common.util.security.provider.AuthenticatedUserProvider;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -10,21 +11,29 @@ import javax.inject.Inject;
 
 @Service
 public class AuthenticatedUserFacade {
+    private final AuthenticatedUserProvider userProvider;
+    private final AuthenticatedUserCache authenticatedUserCache;
+    private TokenStore tokenStore;
 
     @Inject
-    private AuthenticatedUserProvider userProvider;
-    @Inject
-    private AuthenticatedUserCache authenticatedUserCache;
-    @Inject
-    private TokenStore tokenStore;
+    public AuthenticatedUserFacade(final AuthenticatedUserProvider userProvider,
+                                   final AuthenticatedUserCache authenticatedUserCache,
+                                   final TokenStore tokenStore) {
+        this.userProvider = userProvider;
+        this.authenticatedUserCache = authenticatedUserCache;
+        this.tokenStore = tokenStore;
+    }
 
     public void logout(final String accessToken) {
         authenticatedUserCache.delete(accessToken);
+        final String userId = userProvider.userId();
+        final Iterable<AuthenticatedUser> users = authenticatedUserCache.findByUserId(userId);
+        authenticatedUserCache.delete(users);
+
         final OAuth2AccessToken oauth2Token = tokenStore.readAccessToken(accessToken);
         if (oauth2Token != null) {
             tokenStore.removeAccessToken(oauth2Token);
             tokenStore.removeRefreshToken(oauth2Token.getRefreshToken());
         }
     }
-
 }
