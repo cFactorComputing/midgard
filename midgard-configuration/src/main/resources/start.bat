64feceb67a07505e8acefe9e2a89652924b84310
Anoop K John <anoop.kjohn@swiftwallet.io>
2017-03-21 15:27:03 +0530

    Micro service id as application parameter

--- midgard-configuration/src/main/resources/start.bat
+++ midgard-configuration/src/main/resources/start.bat
@@ -11,4 +11,4 @@ IF NOT "%userLogback%"=="" (
     SET logbackLocation=%userLogback%
 )
 echo "java -Xmx2048m -Xms256m -jar %rootpath%swiftwallet-midgard.jar"
-java -Xmx2048m -Xms256m -Dodin.zookeeper.connectString=localhost:2181 -jar %rootpath%swiftwallet-midgard.jar
+java -Xmx2048m -Xms256m -Dodin.zookeeper.connectString=localhost:2181 -Dmicro-service.id=midgard-node1 -jar %rootpath%swiftwallet-midgard.jar
